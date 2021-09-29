2c12ce78c9e5bdb4c980c205a9f30d65420e6b94
adaptive-beaver <adaptive.beaver@gmail.com>
2021-09-29 15:45:23 -0500

    install rn-fetch-blob

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   Button,
 } from 'react-native';
 import { WebView } from 'react-native-webview';
+import RNFetchBlob from 'rn-fetch-blob'
 
 const urlWithLinkToPDF = "https://topherpedersen.blog/2021/09/29/how-to-download-a-pdf-from-a-webview-in-react-native/";
 const pdfURL = "https://archive.org/download/MythicalManMonth/Brooks%201974%20The%20Mythical%20Man-Month-%20Essays%20on%20Software%20Engineering%2C%20Anniversary%20Edition%20%282nd%20Edition%29.pdf";
